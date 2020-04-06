f202c17b257b0f67649396e6a4b96ee379251960
Kirill Timoshenko <kirill.timoshienko@gmail.com>
2020-04-06 13:03:30 +0300

    made coefficients final method that count discriminant now private

--- quadratic-equation/src/QuadraticEquation.java
+++ quadratic-equation/src/QuadraticEquation.java
@@ -1,39 +1,15 @@
 public class QuadraticEquation {
 
-    private double a;
-    private double b;
-    private double c;
+    private final double A;
+    private final double B;
+    private final double C;
     private double firstRoot;
     private double secondRoot;
 
     public QuadraticEquation(double a, double b, double c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-    }
-
-    public double getA() {
-        return a;
-    }
-
-    public void setA(double a) {
-        this.a = a;
-    }
-
-    public double getB() {
-        return b;
-    }
-
-    public void setB(double b) {
-        this.b = b;
-    }
-
-    public double getC() {
-        return c;
-    }
-
-    public void setC(double c) {
-        this.c = c;
+        this.A = a;
+        this.B = b;
+        this.C = c;
     }
 
     public double getFirstRoot() {
@@ -44,21 +20,21 @@ public class QuadraticEquation {
         return secondRoot;
     }
 
-    public double getDiscriminant() {
-        return (Math.pow(b, 2) - 4 * a * c);
-    }
-
     public void solve() throws NegativeDiscriminantException {
-        double discriminant = getDiscriminant();
+        double discriminant = countDiscriminant();
         if (discriminant > 0.0) {
-            firstRoot = (-b + Math.sqrt(discriminant)) / (2 * a);
-            secondRoot = (-b - Math.sqrt(discriminant)) / (2 * a);
+            firstRoot = (-B + Math.sqrt(discriminant)) / (2 * A);
+            secondRoot = (-B - Math.sqrt(discriminant)) / (2 * A);
         } else if (discriminant == 0.0) {
-            firstRoot = -b / (2 * a);
+            firstRoot = -B / (2 * A);
             secondRoot = firstRoot;
         } else {
-            throw (new NegativeDiscriminantException("Negative discriminant." +
-                    " No real roots. Please, check your coefficients."));
+            throw new NegativeDiscriminantException("Negative discriminant." +
+                    " No real roots. Please, check your coefficients.");
         }
+    }
+
+    private double countDiscriminant() {
+        return (Math.pow(B, 2) - 4 * A * C);
     }
 }
